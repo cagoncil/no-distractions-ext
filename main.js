b9e48b48106096cc3de0e509ff4a2fa7fdfbf2d1
cagoncil <crystallophobia@gmail.com>
2022-06-21 16:17:31 -0700

    add new icons and modify js files

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
 document.getElementsByTagName("body")[0].style.display = "none";
-
-// const app = document.getElementById('app');
-// app.parentNode.removeChild('app');
-
-
-// facebook
-// document.getElementById('app-body').style.display = "none";
-
-// instagram
-// document.getElementById('mount_0_0_C8').style.display = "none";
-
-// tiktok
-// document.getElementsByTagName("body")[0].style.display = "none";
